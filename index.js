77dabcc7a2dc7a9d13e161eeb73b2069ac01e44a
Philip <philwarren1@gmail.com>
2021-04-08 06:36:55 -0400

    Completed Assignmennt

---
+++ index.js
@@ -0,0 +1,12 @@
+//     1) has a function expression called divide
+const divide = () => 2000/100
+
+//     2) divide divides 2000 by 100
+
+const square = x => x*x 
+//     3) has an arrow function called square
+//     4) square arrow function takes one parameter and multiplies it times itself
+
+const add = (x,y) => x+y
+//     5) has an arrow function called add
+//     6) add arrow function takes two parameters and adds them together
